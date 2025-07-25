bc691aa45b441c7b6e7160d757a91d2d445cff83
Myleeprita <ultime-sayan@hotmail.fr>
2025-07-25 17:51:17 +0200

    Modification de la racine du projet

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+import Home from './pages/Home';
+import Services from './pages/Services';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Mentions from './pages/Mentions';
+
+
+import logo from './logo.svg';
+import './App.css';
+
+
+
+export default App;
