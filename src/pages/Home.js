d6b9f98037a725b265fd53f9fc210fe5e7677259
Myleeprita <ultime-sayan@hotmail.fr>
2025-07-26 11:31:24 +0200

    Add file assets and images + H1 and H2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,15 @@
+import monImage from '../assets/hero-bg.jpg';
+import '../App.css';
+
 export default function Home() {
-    return <h1>Bienvenue</h1>;
+    return (
+        <div className="position-relative text-center">
+            <img src={monImage} alt="Mon Image" className="img-fluid w-100" />
+
+            <div className="overlay-text">
+                <h1 className="text-color"> Bonjour, je suis John Doe </h1>
+                <h2 className="text-color"> Développeur web full stack </h2>
+            </div>
+        </div>
+    )
 }
