bf52334ca3e0d9fd94686da96c035689afd9eb87
Myleeprita <ultime-sayan@hotmail.fr>
2025-07-26 14:50:51 +0200

    Add url Bootstrap.icon , Add Footer

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
                 <Route path='/contact' element={<Contact />} />
                 <Route path='/mentions' element={<Mentions />} />
             </Routes>
+            <Footer/>
         </Router>
     );
 }
