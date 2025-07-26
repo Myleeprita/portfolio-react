c5d6363e6ab514b6ca4353d817b0eb354ff9d6a1
Myleeprita <ultime-sayan@hotmail.fr>
2025-07-26 10:45:23 +0200

    Add NavBar-responsive Mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,15 @@
 import { Link } from 'react-router-dom';
 
+// BARRE DE NAVIGATION AVEC SON MENU BURGER RESPONSIVE MOBILE
 export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="container">
+            <div className="container-fluid">
                 <Link className="navbar-brand" to="/"> John Doe </Link>
-                <div className="collapse navbar-collapse">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav ms-auto">
                         <li className="nav-item"><Link className="nav-link" to="/"> Home </Link></li>
                         <li className="nav-item"><Link className="nav-link" to="/services"> Services </Link></li>
